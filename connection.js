3144301c8331f40cb08de96aeda0f9974cd90d3c
Christian Käslin <c.kaeslin@gmx.ch>
2017-02-09 12:24:56 +0100

    Env adjustment (#8)
    * adjust env variable for cloud docker and docker-compose usage
    
    * switch to docker-compose/cloud docker style env variable
    
    * use port from couchbase env
    
    * update version and change circle.yml for dev deploy

--- connection.js
+++ connection.js
@@ -1,15 +1,19 @@
 import couchbase from 'couchbase';
 const config = require('./database.json');
 
-if (!process.env.COUCHBASE_HOST) {
-  throw new Error('COUCHBASE_HOST must be specified');
+if (!process.env.COUCHBASE_PORT_8091_TCP_ADDR) {
+  throw new Error('COUCHBASE_PORT_8091_TCP_ADDR must be specified');
+}
+
+if (!process.env.COUCHBASE_PORT_8091_TCP_PORT) {
+  throw new Error('COUCHBASE_PORT_8091_TCP_PORT must be specified');
 }
 
 if (!config.couchbase || !config.couchbase.migrationBucket) {
   throw new Error('Your database.json must have a couchbase section with setting migrationBucket');
 }
 
-const host = `${process.env.COUCHBASE_HOST}:8091?detailed_errcodes=1`;
+const host = `${process.env.COUCHBASE_PORT_8091_TCP_ADDR}:${process.env.COUCHBASE_PORT_8091_TCP_PORT}?detailed_errcodes=1`;
 console.log(`Connecting to ${host}, opening bucket ${config.couchbase.migrationBucket}`);
 const cluster = new couchbase.Cluster(host);
 const bucket = cluster.openBucket(config.couchbase.migrationBucket);
