fa4c77f22b66aa112199eb21443307c7637d7d03
Christian Käslin <c.kaeslin@gmx.ch>
2017-02-14 15:59:58 +0100

    fix env couchbase path

--- connection.js
+++ connection.js
@@ -13,7 +13,7 @@ if (!config.couchbase || !config.couchbase.migrationBucket) {
   throw new Error('Your database.json must have a couchbase section with setting migrationBucket');
 }
 
-const host = `${process.env.COUCHBASE_PORT_8091_TCP_ADDR}:${process.env.COUCHBASE_PORT_8091_TCP_PORT}?detailed_errcodes=1`;
+const host = `couchbase://${process.env.COUCHBASE_PORT_8091_TCP_ADDR}:${process.env.COUCHBASE_PORT_8091_TCP_PORT}?detailed_errcodes=1`;
 console.log(`Connecting to ${host}, opening bucket ${config.couchbase.migrationBucket}`);
 const cluster = new couchbase.Cluster(host);
 const bucket = cluster.openBucket(config.couchbase.migrationBucket);
